17be731279d522f383eb51bdc69ad082112307c0
Arjuna96 <shanilarjuna@gmail.com>
2021-09-23 08:00:17 +0530

    Formatting Changes

--- src/cart.test.js
+++ src/cart.test.js
@@ -1,14 +1,9 @@
 const { addToCart, cleanCart, getTotalPrice, getTotalPriceWithTax, getSalesTaxAmount } = require('./cart');
-// const  = require('./cart');
-
-// let prod1 = { id: 1, name: 'Shower Gel', price: 49.99 };
-// let prod2 = { id: 2, name: 'Deodorant', price: 99.99 };
 
 test('Add 5 Shower Gels, then price to equal 249.95', () => {
   cleanCart();
   addToCart(1, 5);
   expect(getTotalPrice()).toBe(249.95);
-  //   expect(addToCart(2, prod, 3)).toBe(72);
 });
 
 test('Add 3 more Shower Gels, then price to equal 399.92', () => {
@@ -19,7 +14,6 @@ test('Add 3 more Shower Gels, then price to equal 399.92', () => {
 });
 
 test('Add 2 Shower Gels & 2 Deodorants with sales tax price to equal 37.50', () => {
-  //   addToCart(2, prod2, 3);
   cleanCart();
   addToCart(1, 2);
   addToCart(2, 2);
@@ -27,7 +21,6 @@ test('Add 2 Shower Gels & 2 Deodorants with sales tax price to equal 37.50', () 
 });
 
 test('Add 2 Shower Gels & 2 Deodorants with sales tax, then price to equal 337.46', () => {
-  //   addToCart(2, prod2, 3);
   cleanCart();
   addToCart(1, 2);
   addToCart(2, 2);
